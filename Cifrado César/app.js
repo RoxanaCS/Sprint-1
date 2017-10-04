038923c7cc9f8e09846254be6d00badd446b5503
RoxanaCS <roxale.cares@gmail.com>
2017-10-04 01:28:26 -0300

    Cifrado Cesar

---
+++ Cifrado César/app.js
@@ -0,0 +1,7 @@
+var sentence = prompt('Escriba una frase');
+
+var encrypted = function (sentence) {    //transformar la variable de entrada (sentence) en un array para poder manejarlo
+  var array = Array.from(sentence);
+for (var i=0; i=array.length; i++){
+  
+}
