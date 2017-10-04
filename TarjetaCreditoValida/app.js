d224a39c8f36809f966d4d4b3672f3ee1e3dd761
RoxanaCS <roxale.cares@gmail.com>
2017-10-03 23:09:34 -0300

    segunda version

--- TarjetaCreditoValida/app.js
+++ TarjetaCreditoValida/app.js
@@ -1,14 +1,31 @@
 var sentence = prompt('Ingresa el número de tu tarjeta de crédito');
 
-var isValidCard = function (sentence) {    //transformar la variable de entrada (sentence) en un array y darlo vuelta
-var array = Array.from(sentence);
-//console.log(array)
+var isValidCard = function (sentence) {    //transformar la variable de entrada (sentence) en un array de numeros y darlo vuelta
+var array = Array.from(sentence); //transformar la variable de entrada "sentence" en un array
+console.log(array)
 var newArray = [];
-//console.log(array.length)
+console.log(array.length)
 for (var i = 0; i < array.length; i++){
-  newArray.push(parseInt(array[i]));
+  var element = parseInt(array[i]);   //transformar el array compuesto de strings a numbers
+  if (element != NaN) {
+  newArray.push(element);
+}
+else {
+  alert "Por favor ingrese un número válido"
+}
 }
   return newArray.reverse()
+}         //invertir el orden del array de numbers
 
-  //verificar que el residuo de 10 de la suma de todos los números es 0
+  for (var j =1 ; j<newArray.length; j=j+2){  //ubicar los numeros en posición par y multiplicarlos por 2
+  if ((newArray[j]*2) >=10){
+
+  }
+
+     //sumar los digitos del resultado de la multiplicacion de los numeros en ubicacion par por 2 y cambiarlos en el array inverso
+   }
+
+
+ //verificar que el residuo de 10 de la suma de todos los números es 0
+}
 }
