5b30b9d15cdd538f9bfd70c389b45957655b2b42
RoxanaCS <roxale.cares@gmail.com>
2017-10-04 02:19:28 -0300

    cifrado cesar version2

--- Cifrado César/app.js
+++ Cifrado César/app.js
@@ -1,7 +1,17 @@
-var sentence = prompt('Escriba una frase');
+var sentence = prompt('Escriba una frase').toUpperCase();
 
-var encrypted = function (sentence) {    //transformar la variable de entrada (sentence) en un array para poder manejarlo
-  var array = Array.from(sentence);
-for (var i=0; i=array.length; i++){
-  
-}
+var encrypted = function (sentence) {
+var arrayPosition = [];
+var newString =""
+  for (var i=0; i< sentence.length; i++){ //Obtener la posición ASCII del caracter
+    code = sentence.charCodeAt(i);
+    code = (code-65+33) % 26 +65   /* aplicar formula para obtener la posición
+del caracter corrido en 33 espacios a la derecha en la tabla ASCII */
+  arrayPosition.push(String.fromCharCode(code))  //usar la posición para obtener qué letra corresponde
+  newString= arrayPosition.join("") //convertir a string el array
+
+  }
+  return alert (newString);
+  }
+
+  encrypted (sentence);    //llamando a la función
