bc7640caf4f28c447ef0a5f02aaf5b189bc9ee21
RoxanaCS <roxale.cares@gmail.com>
2017-10-05 01:33:58 -0300

    última versión cifrado cesar

--- Cifrado César/app.js
+++ Cifrado César/app.js
@@ -1,21 +1,21 @@
 var initial = function (){
-  do{
+  do{                                                //uso de do while para evaluar la variable de entrada
     var sentence = prompt('Escriba una frase');
     var trueSentence
-    for (var i=0; i<sentence.length; i++){
+    for (var i=0; i<sentence.length; i++){           //recorrer la variable de entrada para saber si hay espacios vacios o números
       code = sentence.charCodeAt(i)
-      if((code>=65 && code <=90) || (code>=97 && code <=122)){
+      if((code>=65 && code <=90) || (code>=97 && code <=122)){   //uso de código ASCII para limitar la entrada solo a letras
         trueSentence = true;
-    } else {
-      trueSentence = false;
-      alert("Ingrese solo letras");
-      i = sentence.length;
+      } else {
+        trueSentence = false;
+        alert("Ingrese solo letras");
+        i = sentence.length;                        //si sentence tiene números o vacios el i se vuelve igual al largo de sentence y el for se detiene
+      }
     }
-  }
-} while (trueSentence == false);
+  } while (trueSentence == false);          //el prompt se ejecuta repetidamente mientras que el usuario no ingrese solo letras
 
-principal2(sentence);
-}
+  principal2(sentence);                    //Llama a la función de más abajo que contiene el cifrar/decifrar
+  }
 
 
 var principal2 = function(sentence2){    //funcion para elegir cifrar o descifrar
@@ -23,17 +23,17 @@ var principal2 = function(sentence2){    //funcion para elegir cifrar o descifra
 		var respuesta = prompt("Indique el número de lo que desea hacer: 1)Cifrar - 2)Descifrar");
 		if(respuesta != ""){
 			if(respuesta == "1") {
-				cipher(sentence2);
+				cipher(sentence2);            //si la respuesta es cifrar, se ejecutará la función cipher
 			} else if (respuesta == "2") {
-				decipher(sentence2);
+				decipher(sentence2);           //si la respuesta es descifrar, se ejecutará la función decipher
 			} else {
 				alert("Ingrese una opción válida");
 			}
 		}
-	} while (respuesta == "" || (respuesta != "1" && respuesta != "2"));
+	} while (respuesta == "" || (respuesta != "1" && respuesta != "2"));    //el prompt se ejecuta repetidamente mientras el usuario no ingrese una opción válida
 }
 
-var cipher = function (sentence2) {
+var cipher = function (sentence2) {   //función para cifrar
 var arrayPosition = [];
 var newString = "";
 var validLetter;
@@ -45,16 +45,16 @@ var code;
     } else {
       validLetter =97;
     }
-    code = (code- validLetter + 33) % 26 +validLetter;   /* aplicar formula para obtener la posición
+    code = (code- validLetter + 33) % 26 +validLetter;   /* aplicar fórmula para obtener la posición
 del caracter corrido en 33 espacios a la derecha en la tabla ASCII */
-  arrayPosition.push(String.fromCharCode(code));  //usar la posición para obtener qué letra corresponde
-  newString= arrayPosition.join(""); //convertir a string el array
+  arrayPosition.push(String.fromCharCode(code));  //usar la posición para obtener qué letra corresponde y guardar las letras en un array
+  newString= arrayPosition.join(""); //convertir el array en string
 
   }
   return alert (newString);
   }
 
-  var decipher = function (sentence3) { // Funcion para descifrar
+  var decipher = function (sentence3) { // Función para descifrar
   var arrayPosition = [];
   var newString ="";
   var validLetter;
@@ -67,19 +67,17 @@ del caracter corrido en 33 espacios a la derecha en la tabla ASCII */
         console.log("es mayuscula")
       } else {
         validLetter =97;
-        console.log("no es mayuscula")
       }
-      console.log(code)
-      code = (code + validLetter - 33) % 26 + validLetter;   /* aplicar formula para obtener la posición
+
+      code = code - (33 % 26);  /* aplicar fórmula para obtener la posición
   del caracter corrido en 33 espacios a la izquierda en la tabla ASCII */
-  console.log((code+validLetter-33)%26)
-  console.log(code)
-    arrayPosition.push(String.fromCharCode(code));  //usar la posición para obtener qué letra corresponde
+
+    arrayPosition.push(String.fromCharCode(code));  //usar la posición para obtener qué letra corresponde y guardar las letras en un array
     newString= arrayPosition.join(""); //convertir a string el array
-  console.log(newString)
+
     }
     return alert (newString);
 
     }
 
-initial();
+initial();   //llamar a la función que da inicio a todo el proceso
